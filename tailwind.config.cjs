dbc9c15c8ccb6a629dccf821a3967b82d9224d01
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-11-29 11:14:34 +0000

    fix(code): refactoring and optimizations

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./webapp/**/*.{html,js,svelte,ts,json}'],
+  content: ['./src-web/**/*.{html,js,svelte,ts,json}'],
   theme: {
     extend: {},
   },
