631738c55b8d8da2e07035b6d7f7c941ea6ff520
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-12-17 08:25:58 +0000

    fix(license): switched to the correct license url

--- src-tauri/src/core/constants.rs
+++ src-tauri/src/core/constants.rs
@@ -23,7 +23,7 @@ pub const ACKNOWLEDGEMENTS: &str =
     "https://github.com/sebastienrousseau/password-generator-pro/graphs/contributors";
 pub const DOCUMENTATION: &str = "https://password-generator.pro/docs";
 pub const LICENSE_URL: &str =
-    "https://github.com/sebastienrousseau/password-generator-pro/blob/main/LICENSE";
+    "https://github.com/sebastienrousseau/password-generator-pro/blob/main/COPYRIGHT";
 pub const RELEASE: &str = "https://github.com/sebastienrousseau/password-generator-pro/releases";
 pub const ISSUE: &str = "https://github.com/sebastienrousseau/password-generator-pro/issues";
 
