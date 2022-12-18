64e79f10d7329d0fa7c4ae0ff3b98d0a8a7acd82
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-12-18 10:25:18 +0000

    feat(logger): updated logger

--- src-tauri/src/core/logger.rs
+++ src-tauri/src/core/logger.rs
@@ -1,15 +1,15 @@
-//! # Utilities for logging messages to the console.
-//! An easy-to-configure and flexible logger function.
-//! Log a message to the console with a timestamp, info, message, and details.
+//! # Logger
+//!
+//! Logger provides an opinionated logger with a simple, readable output format.
 //!
 //! # Usage
 //!
 //! ```rust
 //! use crate::core::logger;
 //!
-//! // Log a message to the console with a timestamp, info, message, and details
+//! // Log a message to the console with a timestamp, info, message, and details to stderr.
 //! let logger = logger::logger(
-//!     "2022-12-18 2:02:08.706196 +00:00:00",
+//!     "2022-22-22 22:22:22.222222 +00:00:00",
 //!     "INFO",
 //!     "SystemTrayEvent",
 //!     "Showing main window"
@@ -21,15 +21,29 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-License-Identifier: MIT
 
-#[tauri::command]
-pub fn set_log(time: &str, info: &str, message: &str, details: &str) {
-    println!(
-        "🔒 {} - [{}] {}: {}",
-        time,
-        info,
-        message,
-        details
-    );
+pub struct Logger {
+    time: String,
+    info: String,
+    message: String,
+    details: String,
+}
+
+impl Logger {
+    pub fn new(time: &str, info: &str, message: &str, details: &str) -> Self {
+        Self {
+            time: time.to_string(),
+            info: info.to_string(),
+            message: message.to_string(),
+            details: details.to_string(),
+        }
+    }
+
+    pub fn log(&self) {
+        println!(
+            "🔒 {} - [{}] {}: {}",
+            self.time, self.info, self.message, self.details
+        );
+    }
 }
 
 #[cfg(test)]
@@ -38,11 +52,52 @@ mod tests {
 
     #[test]
     fn test_logger() {
-        let logger = set_log(
+        let logger = Logger::new(
             "2022-22-22 22:22:22.222222 +00:00:00",
             "INFO",
             "SystemTrayEvent",
-            "Showing main window");
-        assert_eq!(logger, ());
+            "Showing main window",
+        );
+        assert_eq!(logger.time, "2022-22-22 22:22:22.222222 +00:00:00");
+        assert_eq!(logger.info, "INFO");
+        assert_eq!(logger.message, "SystemTrayEvent");
+        assert_eq!(logger.details, "Showing main window");
+    }
+
+    #[test]
+    fn test_logger_log() {
+        let logger = Logger::new(
+            "2022-22-22 22:22:22.222222 +00:00:00",
+            "INFO",
+            "SystemTrayEvent",
+            "Showing main window",
+        );
+        logger.log();
     }
 }
+
+
+// pub fn set_log(time: &str, info: &str, message: &str, details: &str) {
+//     println!(
+//         "🔒 {} - [{}] {}: {}",
+//         time,
+//         info,
+//         message,
+//         details
+//     );
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn test_logger() {
+//         let logger = set_log(
+//             "2022-22-22 22:22:22.222222 +00:00:00",
+//             "INFO",
+//             "SystemTrayEvent",
+//             "Showing main window");
+//         assert_eq!(logger, ());
+//     }
+// }
