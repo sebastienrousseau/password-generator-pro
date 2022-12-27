e1cda98119061a88aea06eed6f69d68e7b636320
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-12-27 10:18:14 +0000

    test(qrcode): Added test_export

--- src-tauri/src/util/qrcode.rs
+++ src-tauri/src/util/qrcode.rs
@@ -2,6 +2,16 @@
 //!
 //! QRCode provides a simple way to generate a new QRCode.
 //!
+//! # Examples
+//!
+//! ```
+//! use qrcode::QrCode;
+//!
+//! let qrcode = QrCode::new("Hello World");
+//!
+//! ```"
+//!
+//!
 
 use qrcode::{render::svg, QrCode};
 use std::fs;
@@ -9,10 +19,11 @@ use std::fs;
 /// QRCode Utility
 ///
 /// By default, a new QRCode is generated.
+///
+///
 #[non_exhaustive]
-#[derive(Debug, Clone, Default, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[derive(Debug, Clone, Default, PartialEq)]
 pub struct QRCode;
-
 
 impl QRCode {
     /// Generate a new QRCode from the text.
@@ -27,6 +38,7 @@ impl QRCode {
     /// use util::qrcode::QRCode;
     ///
     /// let qrcode = QRCode::new("Hello World!");
+    ///
     /// ```
     pub fn qrcode(content: &str) -> String {
         let code = QrCode::new(content).unwrap();
@@ -38,44 +50,10 @@ impl QRCode {
             .light_color(svg::Color("#ffffff"))
             .build();
         return svg_data.to_string();
-
-
-
-
-        // Ok(())
-
-        // let code = QrCode::new(content).unwrap();
-        // let svg_data = code
-        //     .render()
-        //     .min_dimensions(300, 300)
-        //     .max_dimensions(300, 300)
-        //     .dark_color(svg::Color("#000000"))
-        //     .light_color(svg::Color("#ffffff"))
-        //     .build();
-
-
-
-        // return svg_data.to_string();
-        // println!("{}", image);
-            // println!("{}", image);
     }
 
-
     pub fn export(content: &str, name: &str) {
-        let code = QrCode::new(content).unwrap();
-        let svg_data = code
-            .render()
-            .min_dimensions(300, 300)
-            .max_dimensions(300, 300)
-            .dark_color(svg::Color("#000000"))
-            .light_color(svg::Color("#ffffff"))
-            .build();
-
-        fs::write(name, svg_data).expect("Unable to write file");
-
-        println!("QRCode generated!");
-
-
+        fs::write(name, QRCode::qrcode(content)).unwrap();
     }
 }
 
@@ -107,5 +85,13 @@ mod tests {
         let qrcode = QRCode::qrcode("Hello World!");
         let qrcode_clone = qrcode.clone();
         assert_eq!(qrcode, qrcode_clone);
+    }
+
+    #[test]
+    fn test_export() {
+        QRCode::export("Hello World!", "test_qrcode.svg");
+        assert_eq!(true, true);
+        fs::remove_file("test_qrcode.svg").unwrap();
+        assert_eq!(false, false);
     }
 }
